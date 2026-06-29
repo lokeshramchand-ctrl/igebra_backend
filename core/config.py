4294ebd4110072684c0448c9546cce76732d1e53
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-06-29 10:51:18 +0530

    feat: Update Milvus and Ollama URIs in settings for local development

--- core/config.py
+++ core/config.py
@@ -3,8 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     MONGODB_URI: str
     MONGODB_DB_NAME: str = "velar"
-    MILVUS_URI: str = "https://milvus.lokeshrc.me:443"
-    OLLAMA_URI: str = "http://ollama:11434"   # <-- add this
+    MILVUS_URI: str = "http://localhost:19530"
+    OLLAMA_URI: str = "http://localhost:11434"   # <-- add this
+    velar_api_key: str
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
